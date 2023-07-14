ebdacf5d643655a01b51689355db7876d8ebe6c0
Eduardo Marioti <edumarioti@gmail.com>
2023-07-14 10:26:57 -0300

    lendo arquivos e tratando erros

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-import chalk from "chalk";
+import chalk from "chalk"
+import fs from 'fs'
 
+function trataErro(erro) {
+    let mensagemErro = ''
 
-console.log(chalk.blue('olá mundo'));
+    if (erro.code == 'ENOENT') {
+        mensagemErro = '-> Não foi encontrado o arquivo'
+    } else if (erro.code == 'EISDIR') {
+        mensagemErro = '-> Foi informado um caminho não aponta para um arquivo'
+    } 
+
+    throw new Error(chalk.red(erro.code, mensagemErro))
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encode = 'utf-8'
+    fs.readFile(caminhoDoArquivo, encode, (erro, texto) => {
+        if (erro){
+            console.log(erro)
+            trataErro(erro)
+        }
+            
+        console.log(chalk.green(texto))
+    })
+}
+
+pegaArquivo('./arquivos/texto.md')
